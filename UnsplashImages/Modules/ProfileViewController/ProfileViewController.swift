9b366eadeee90b87664aa1275863089ec6ea29db
Dmitry <otlichnik24@yandex.ru>
2024-02-22 09:05:57 +0200

    final 12 print final

--- UnsplashImages/Modules/ProfileViewController/ProfileViewController.swift
+++ UnsplashImages/Modules/ProfileViewController/ProfileViewController.swift
@@ -81,7 +81,9 @@ final class ProfileViewController: UIViewController {
     
     private func clean() {
         HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
-        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+        WKWebsiteDataStore.default().fetchDataRecords(
+            ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()
+        ) { records in
             records.forEach { record in
                 WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
             }
